50739bf51632e1fe2e814ee615fca48a1d0dd0d8
Aung Kyaw Nyunt <akn.zotefamily@gmail.com>
2022-02-16 00:18:47 +0630

    [create]

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -38,7 +38,7 @@ export default function Header({...props }) {
             rel="stylesheet"
             href="https://use.fontawesome.com/releases/v5.13.1/css/all.css"
             integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
             />
         </Head>
     )
