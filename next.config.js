e372db7add1be730556ecc0b5da016b192b6d9a4
Aung Kyaw Nyunt <akn.zotefamily@gmail.com>
2022-02-28 18:01:10 +0630

    [modify]  redirect

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@
 //   NEXT_PUBLIC_ImageDomain:  process.env.NEXT_PUBLIC_ImageDomain
 // }
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://srv.adstxtmanager.com/29571/aungkyawnyunt.com',
+        permanent: true,
+      },
+    ]
+  },
   compilerOptions: {
    noImplicitReturns: true,
    noImplicitAny: false,
